0c3cd966c979580e3ac6b35bc5bfeb3a09ed9027
Pravas Paudel <ppravas67@gmail.com>
2024-11-01 16:56:06 +0545

    commit

--- 02/server/server.js
+++ 02/server/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Use the CORS middleware
 app.use(cors());
 
-const Port = process.env.Port || 5000; // Default to port 5000 if not set
+const Port = process.env.Port || 5000;
 
 app.listen(Port, () => {
   console.log(`Port is listening on port ${Port}`);
